9969b19d2eb768cdb8ca8a1bacd778eb3346eb90
dimitriskouz <dimitrismosh@gmail.com>
2016-11-08 16:55:32 +0200

    update
    Login Button

--- RPGo/app/src/main/java/com/example/mental/rpgo/DatabaseHelper.java
+++ RPGo/app/src/main/java/com/example/mental/rpgo/DatabaseHelper.java
@@ -1,6 +1,7 @@
 package com.example.mental.rpgo;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -29,6 +30,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS contacts");
+            onCreate(db);
+        }
 
+    public Cursor getData(String name, String pwd) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select id from user where name='"+name+"' AND password='"+pwd+"'", null );
+        return res;
     }
+
+
 }
