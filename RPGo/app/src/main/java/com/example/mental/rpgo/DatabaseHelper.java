3524ab409b6d546d641f28212c1bf97ba913e434
angelapapado <angelapapado1993@hotmail.com>
2016-11-14 19:58:57 +0200

    test
    tests

--- RPGo/app/src/main/java/com/example/mental/rpgo/DatabaseHelper.java
+++ RPGo/app/src/main/java/com/example/mental/rpgo/DatabaseHelper.java
@@ -42,22 +42,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
-    public boolean insert_user(String name, String pwd) {
-
-        boolean insertSuccessful = false;
-
-        ContentValues values = new ContentValues();
-
-
-        values.put(USER_COLUMN_NAME, name);
-        values.put(USER_COLUMN_PASSWORD, pwd);
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        insertSuccessful = db.insert(USER_TABLE_NAME, null, values) > 0;
-        // insertSuccessful=false;
-        db.close();
-
-        return insertSuccessful;
     }
 }
