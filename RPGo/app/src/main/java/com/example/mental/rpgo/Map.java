031bbbaf9f4d6c0b84887dc17e71ec526050953f
zoiderv <zoidervidou@gmail.com>
2016-11-22 15:51:31 +0200

    Add_picture

--- RPGo/app/src/main/java/com/example/mental/rpgo/Map.java
+++ RPGo/app/src/main/java/com/example/mental/rpgo/Map.java
@@ -8,9 +8,13 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Map extends FragmentActivity implements OnMapReadyCallback {
 
@@ -51,6 +55,10 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         }
 
         mMap.setMyLocationEnabled(true);
+
+
+
+        
 
     }
 }
