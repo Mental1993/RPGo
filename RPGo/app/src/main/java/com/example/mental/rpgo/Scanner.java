f050f40168a0d089aa17c2d45a25889cf4ba97b3
zoiderv <zoidervidou@gmail.com>
2016-11-27 20:30:47 +0200

    Manifest Additions

---
+++ RPGo/app/src/main/java/com/example/mental/rpgo/Scanner.java
@@ -0,0 +1,11 @@
+package com.example.mental.rpgo;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Zoi on 27/11/2016.
+ */
+
+public class Scanner extends AppCompatActivity {
+}
