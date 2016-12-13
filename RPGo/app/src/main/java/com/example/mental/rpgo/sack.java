86ebf73611d1f83a12be6594cf58a1d831c6a02e
angelapapado <angelapapado1993@hotmail.com>
2016-12-13 16:26:40 +0200

    sack_class1

--- RPGo/app/src/main/java/com/example/mental/rpgo/sack.java
+++ RPGo/app/src/main/java/com/example/mental/rpgo/sack.java
@@ -1,6 +1,22 @@
 package com.example.mental.rpgo;
 
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class sack {
+public class sack extends AppCompatActivity {
+
+    TextView knife,picklock;
+    ImageView impick,imknife;
+    LinearLayout Lpick,Lknife;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sack);
+
+    }
 }
