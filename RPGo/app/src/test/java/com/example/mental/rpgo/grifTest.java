49759dd9b9998f137acdd6ae0e7fd43cf5fac8a1
Thanpets <petfountas@hotmail.com>
2016-12-21 12:01:57 +0200

    testTrim

--- RPGo/app/src/test/java/com/example/mental/rpgo/grifTest.java
+++ RPGo/app/src/test/java/com/example/mental/rpgo/grifTest.java
@@ -49,4 +49,13 @@ public class grifTest extends TestCase{
         }
         assertEquals(cor,trg);
     }
+    @Test
+    public void testTrim(){
+
+        String sol3="     bat    ";
+        if(sol3.trim().equals("bat")){
+            cor=true;
+        }
+        assertEquals(cor,trg);
+    }
 }
